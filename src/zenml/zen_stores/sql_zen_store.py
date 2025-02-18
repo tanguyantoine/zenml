cbc0ba7514bf08add17bf1f54500a81e59dc25fb
Stefan Nica <stefan@zenml.io>
2025-02-18 10:00:57 +0100

    Implement RBAC scoping for workspaces

--- src/zenml/zen_stores/sql_zen_store.py
+++ src/zenml/zen_stores/sql_zen_store.py
@@ -11372,7 +11372,7 @@ class SqlZenStore(BaseZenStore):
                 resource_type = self._get_taggable_resource_type(
                     resource=resource
                 )
-                self.create_tag_resource(
+                self._create_tag_resource(
                     TagResourceRequest(
                         tag_id=tag.id,
                         resource_id=resource.id,
@@ -11401,7 +11401,7 @@ class SqlZenStore(BaseZenStore):
             except KeyError:
                 tag = self.create_tag(TagRequest(name=tag_name))
             try:
-                self.create_tag_resource(
+                self._create_tag_resource(
                     TagResourceRequest(
                         tag_id=tag.id,
                         resource_id=resource_id,
@@ -11583,7 +11583,7 @@ class SqlZenStore(BaseZenStore):
     # Tags <> resources
     ####################
 
-    def create_tag_resource(
+    def _create_tag_resource(
         self, tag_resource: TagResourceRequest
     ) -> TagResourceResponse:
         """Creates a new tag resource relationship.
